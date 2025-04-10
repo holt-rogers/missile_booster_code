62a4d5ae9c417fbac5b21a8cbce93fbcc27c4b65
Ethan Rogers <ehp;trpgers@gmail.com>
2025-04-10 15:45:41 -0400

    implimented booster stage optimization

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import dearpygui.dearpygui as dpg
-from missile_optimization import optimize_mass_ratio, delta_v, calculate_propellent_mass, find_structure_mass
+from missile_optimization import *
 
 #im hacking you rn
 dpg.create_context()
@@ -14,8 +14,8 @@ isp = 300
 structural_efficiency = 4
 
 graph = None
-mass_r1, mass_r2, mass_r3 = 0,0,0
-bmass_r1, bmass_r2, bmass_r3 = 0.6, 0.3, 0.1
+stage_r1, stage_r2, stage_r3 = 0,0,0
+bstage_r1, bstage_r2, bstage_r3 = 0.2, 0.5, 0.3
 
 table = []
 total_v = None
@@ -23,7 +23,7 @@ total_v = None
 
 
 def on_click():
-    global mass_r1, mass_r2, mass_r3, isp, payload, time_to_burn, table
+    global stage_r1, stage_r2, stage_r3, isp, payload, time_to_burn, table, bstage_r1, bstage_r2, bstage_r3
     global diameter, height, density, structural_efficiency
 
     isp = dpg.get_value("isp")
@@ -34,19 +34,20 @@ def on_click():
     diameter = dpg.get_value("diameter")
     structural_efficiency = dpg.get_value("efficiency")
 
-
+    
     propllent_mass = calculate_propellent_mass(height, diameter, density)
     structure_mass = find_structure_mass(structural_efficiency, payload, propllent_mass)
 
-    mass_r1, mass_r2, mass_r3 = optimize_mass_ratio(isp, payload, structure_mass, propllent_mass, graph=graph)
-    mass_r1, mass_r2, mass_r3 = round(mass_r1, 3), round(mass_r2,3), round(mass_r3,3)
+    bstage_r1, bstage_r2, bstage_r3 = optimize_booster(isp, payload, structure_mass, propllent_mass, time_to_burn)
+    stage_r1, stage_r2, stage_r3 = optimize_mass_ratio(isp, payload, structure_mass, propllent_mass, graph=graph)
+    stage_r1, stage_r2, stage_r3 = round(stage_r1, 3), round(stage_r2,3), round(stage_r3,3)
     update_mass_ratio_visual()
     update_table()
     
 
 
 def update_mass_ratio_visual():
-    global mass_r1, mass_r2, mass_r3, bmass_r1, bmass_r2, bmass_r3
+    global stage_r1, stage_r2, stage_r3, bstage_r1, bstage_r2, bstage_r3
 
     pos_x = 10
     pos_y = 10
@@ -56,13 +57,13 @@ def update_mass_ratio_visual():
 
 
     pos3min = [pos_x, pos_y]
-    pos3max = [pos_x + width, pos_y + mass_r3*height]
+    pos3max = [pos_x + width, pos_y + stage_r3*height]
 
     pos2min = [pos_x, pos3max[1]]
-    pos2max = [pos_x + width, pos2min[1] + height * mass_r2]
+    pos2max = [pos_x + width, pos2min[1] + height * stage_r2]
 
     pos1min = [pos_x, pos2max[1]]
-    pos1max = [pos_x + width, pos1min[1] + height * mass_r1]
+    pos1max = [pos_x + width, pos1min[1] + height * stage_r1]
 
     border_color = (0,0,0,255)
 
@@ -86,13 +87,13 @@ def update_mass_ratio_visual():
 
 
     pos3min = [pos_x, pos_y]
-    pos3max = [pos_x + width, pos_y + mass_r3*height]
+    pos3max = [pos_x + width, pos_y + bstage_r3*height]
 
     pos2min = [pos_x, pos3max[1]]
-    pos2max = [pos_x + width, pos2min[1] + height * mass_r2]
+    pos2max = [pos_x + width, pos2min[1] + height * bstage_r2]
 
     pos1min = [pos_x, pos2max[1]]
-    pos1max = [pos_x + width, pos1min[1] + height * mass_r1]
+    pos1max = [pos_x + width, pos1min[1] + height * bstage_r1]
 
     border_color = (0,0,0,255)
 
@@ -110,21 +111,24 @@ def update_mass_ratio_visual():
 
 
 def update_table():
-    global mass_r1, mass_r2, mass_r3, isp, table, total_v, payload
+    global stage_r1, stage_r2, stage_r3, isp, table, total_v, payload
     global diameter,height,structural_efficiency,density
 
     mp = calculate_propellent_mass(height, diameter, density)
     ms = find_structure_mass(structural_efficiency, payload, mp)
     #
-    v1, v2, v3 = delta_v(mass_r1, mass_r2, mass_r3, isp, payload, ms, mp)
+    v1, v2, v3 = delta_v(stage_r1, stage_r2, stage_r3, isp, payload, ms, mp)
     v1, v2, v3 = round(v1, 2), round(v2, 2), round(v3, 2)
     dv = v1 + v2 + v3
     dv = round(dv, 2)
 
+    mr1, mr2, mr3 = mass_ratios(stage_r1, stage_r2, stage_r3, payload, ms, mp)
+    mr1, mr2, mr3 = round(mr1, 3), round(mr2, 3), round(mr3, 3)
+
     dat = [
-        [3, mass_r3, v3],
-        [2, mass_r2, v2],
-        [1, mass_r1, v1]
+        [3, mr1, v3],
+        [2, mr2, v2],
+        [1, mr3, v1]
     ]
 
     for i in range(2,-1,-1):
@@ -144,8 +148,8 @@ with dpg.window(label="Graphs", no_resize=True, no_close=True, no_move=True, no_
 propllent_mass = calculate_propellent_mass(height, diameter, density)
 structure_mass = find_structure_mass(structural_efficiency, payload, propllent_mass)
 
-mass_r1, mass_r2, mass_r3 = optimize_mass_ratio(isp, payload, structure_mass, propllent_mass, graph=graph)
-mass_r1, mass_r2, mass_r3 = round(mass_r1, 3), round(mass_r2,3), round(mass_r3,3)
+stage_r1, stage_r2, stage_r3 = optimize_mass_ratio(isp, payload, structure_mass, propllent_mass, graph=graph)
+stage_r1, stage_r2, stage_r3 = round(stage_r1, 3), round(stage_r2,3), round(stage_r3,3)
 # window for settings
 with dpg.window(label="Optomization Settings", no_resize=True, no_close=True, no_move=True, no_collapse=True, min_size=[100,250], no_title_bar=False):
     #dpg.add_text("Mass Ratio Optomization For a 3-Stage Rocket")
@@ -163,12 +167,16 @@ with dpg.window(label="Optomization Settings", no_resize=True, no_close=True, no
         dpg.add_input_float(label="Stack Hegith (m)", width=100, step = 0, default_value=height, min_value=0, min_clamped=True, tag = "height")
         dpg.add_input_float(label="Rocket Diameter (m)", width=100, step = 0, default_value=diameter, min_value=0, min_clamped=True, tag = "diameter")
         dpg.add_input_float(label="Pop-out burn time (s)", width=100, step = 0, default_value=time_to_burn, min_value=0.001, min_clamped=True, tag = "burn_time")
+    
+    with dpg.tree_node(label = "Constraints"):
+        dpg.add_input_float(label = "Minimum stage proportion: ")
+        dpg.add_input_float(label = "Maximum stage proportion: ")
         
 
 
 with dpg.window(label="Rocket Visualizations", no_resize=True, no_close=True, no_move=True, no_collapse=True, min_size=[150,250], pos=[0, 250], no_focus_on_appearing=True):
     # draw mass ratio representations
-    with dpg.drawlist(width=191, height=150):
+    with dpg.drawlist(width=191, height=200):
         # Draw a rectangle from (50, 50) to (200, 200)
 
         # add values but tag are calculated and overriden in update_mass_ratio_visual
--- src/missile_optimization.py
+++ src/missile_optimization.py
@@ -1,4 +1,4 @@
-from math import log, pi
+from math import log, pi, e
 import dearpygui.dearpygui as dpg
 
 
@@ -9,7 +9,10 @@ def calculate_propellent_mass(height, diameter, density):
 def find_structure_mass(efficiency, payload, propellent):
     return (propellent + payload)/ efficiency
 
-def optimize_mass_ratio(isp, payload, mass_structure, mass_propellant ,booster = False, time_to_burn = None, graph = None):
+
+# this functions returns the proportion of each size
+# the name is really misleading, my bad
+def optimize_mass_ratio(isp, payload, mass_structure, mass_propellant, graph = None):
 
     best_v = float("-inf") 
     best_ratios = []
@@ -17,24 +20,22 @@ def optimize_mass_ratio(isp, payload, mass_structure, mass_propellant ,booster =
     speeds = []
     ratios = []
 
-    if booster:
-        return 0.2, 0.5, 0.3
-    else:
-        # looping through the possible range of fuel we can use 
-        for mr1 in range(334, 999):
-            for mr2 in range(1, 1000 - mr1):
-                mr3 = 1000 - mr1 - mr2
-                # first has to be largest
 
-                mr1_2, mr2_2, mr3_2 = mr1/1000, mr2/1000, mr3/1000
-                v = sum(list(delta_v(mr1_2, mr2_2, mr3_2, isp, payload, mass_structure, mass_propellant)))
-                if v > best_v:
-                    best_v = v
-                    best_ratios = [mr1_2, mr2_2, mr3_2]
+    # looping through the possible range of fuel we can use 
+    for mr1 in range(334, 999):
+        for mr2 in range(1, 1000 - mr1):
+            mr3 = 1000 - mr1 - mr2
+            # first has to be largest
 
-                speeds.append(v)
-                ratios.append(mr1)
-        return best_ratios
+            mr1_2, mr2_2, mr3_2 = mr1/1000, mr2/1000, mr3/1000
+            v = sum(list(delta_v(mr1_2, mr2_2, mr3_2, isp, payload, mass_structure, mass_propellant)))
+            if v > best_v:
+                best_v = v
+                best_ratios = [mr1_2, mr2_2, mr3_2]
+
+            speeds.append(v)
+            ratios.append(mr1)
+    return best_ratios
 
 '''
         if graph != None:
@@ -45,7 +46,28 @@ def optimize_mass_ratio(isp, payload, mass_structure, mass_propellant ,booster =
         return best_ratios
 '''
 
-# note m1, m2 and m3 are equal to 
+def optimize_booster(isp, payload, mass_structure, mass_propellant, time_to_burn, graph = None):
+    # calculate mass ratio of the booster stage
+    mr1 = (mass_propellant + mass_structure + payload) * (1  - e**(-time_to_burn/(2*isp))) / mass_propellant
+
+    best_v = float("-inf")
+    best_ratios = []
+
+    for proportion in range(1, 990):
+        mr2 = proportion/1000
+        mr3 = 1 - mr2 - mr1
+
+        v = sum(list(delta_v(mr1, mr2, mr3, isp, payload, mass_structure, mass_propellant)))
+
+        if v > best_v:
+            best_v = v
+            best_ratios = [mr1, mr2, mr3]
+    print(*best_ratios)
+    return best_ratios
+
+
+
+
 
 def delta_v(mr1, mr2, mr3, isp, payload, mass_structure, mass_propellent):
     g =  9.80665
@@ -69,4 +91,24 @@ def delta_v(mr1, mr2, mr3, isp, payload, mass_structure, mass_propellent):
 
 
     return v1, v2, v3
+
+
+def mass_ratios(stage1, stage2, stage3, payload, mass_structure, mass_propellent):
+    pl = payload
+    mp1 = stage1 * mass_propellent
+    mp2 = stage2 * mass_propellent
+    mp3 = stage3 * mass_propellent
+
+    ms1 = stage1 * mass_structure
+    ms2 = stage2 * mass_structure
+    ms3 = stage3 * mass_structure
+
+
+    mr3 = 1 + mp3/(ms3 + pl)
+    mr2 = 1 + mp2/(mp3 + ms3 + ms2 + pl)
+    mr1 = 1 + mp1/(mp3 + ms3 + mp2 + ms2 + ms1 + pl)
+
+    return mr1, mr2, mr3
+
+
 
